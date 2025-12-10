9357e8869267345f4564e965a3922e1b4200e1c1
sohelr-dev <sohe.4273@gmail.com>
2025-12-11 03:24:39 +0600

    usersController

--- banking-system-api/routes/api.php
+++ banking-system-api/routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
